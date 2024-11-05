4b1105d992398765daceb45d9f2c995fa6b31ac9
itsjubayer <jubayer.ah@gmail.com>
2024-11-05 15:53:38 +0600

    Add class7

---
+++ 6/FirstProgram.py
@@ -0,0 +1,12 @@
+name = "Shradha"
+age = 23
+price = 25.99
+
+print("my name is : ", name)
+print("my age is : ", age)
+
+
+print(type(name))
+print(type(age))
+print(type(price))
+print(type(True))
---
+++ 6/class7.py
@@ -0,0 +1,107 @@
+# my_data = "Hello"
+# # my_data[0] = 'k'
+# print(my_data[0])
+
+# my_data = [1,2,3,3,4,2]
+# print(list(set(my_data))) #No duplicate data
+
+#----List -Mutable
+# fruits = ["apple", "banana", "cherry"]
+# print(fruits)
+# fruits.append("date")
+# print(fruits)
+
+#------Dictionary -Dict  ----Mutable
+# student = {"name": "Alice", "age":25}
+# print(student)
+# student["grade"]="A"
+# print(student)
+
+#---Set --- -Mutable
+# unique_numbers = {1,2,3}
+# print(unique_numbers)
+# unique_numbers.add(4)
+# print(unique_numbers)
+
+# ------Integer (int) ----Inmutable
+# x=10
+# print(x)
+# x=x+5
+# print(x)
+
+#---Float ---
+# pi=3.14
+# print(pi) 
+# pi = pi+0.00816
+# print(pi)
+
+#---- String(str)
+# greeting = "Hello"
+# print(greeting)
+# greeting = "h"+greeting[1:]
+# print(greeting)
+
+#----------Tuple----
+# colors=("red", "green", "blue")
+# ##colors[1] = "Yellow"
+# print(colors)
+# #index
+# x=(12, 3)
+# ##x[1] = 5
+# print(x)
+
+#--------Range(range)
+# numbers = range(1,5)
+# print(list(numbers))
+
+
+# greeting ='Hello'
+# greeting ="Hello"
+# greeting ='''Hello'''
+# greeting ='''Hello
+#                 Hello
+#                 Hello
+#                 Hello
+#                 Hello
+#                 Hello
+#                 Hello
+#             '''
+# greeting ="""Hello
+# Hello
+#                 Hello
+#                 Hello
+#                 Hello
+#                 Hello
+#                 Hello"""
+# print(greeting)
+
+#--Length
+# greetings = "Hello there!"
+# print(len(greetings))
+# print(greetings[4])
+# print(greetings[:5])
+# print(greetings[1:5])
+
+# greetings="Hello my friend"
+# name = "Nure"
+# print(greetings + ' ' +name)
+# print(greetings.upper())
+# print(greetings.lower())
+# print(greetings.title())
+# print(greetings.strip())
+# print(greetings.strip().upper())
+# print(greetings.lower().find("friend"))
+# print('friend' in greetings)
+
+words ="hello, there, my, friend"
+print(words.split(","))
+print(" ".join(words))
+
+age = 14
+name = 'Nure'
+print(f"Hello, my name is {name}, my age is {age}")
+
+#reverse
+name ='dog'
+x=name[::-1]
+print(x)
