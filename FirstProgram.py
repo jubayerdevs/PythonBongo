57391182445b460be98a962b3f4e1c06e6c7fd2e
itsjubayer <jubayer.ah@gmail.com>
2024-11-10 23:28:34 +0600

    python_list.py added

---
+++ FirstProgram.py
@@ -0,0 +1,30 @@
+# name = "Shradha"
+# age = 23
+# price = 25.99
+
+# print("my name is : ", name)
+# print("my age is : ", age)
+
+
+# print(type(name))
+# print(type(age))
+# print(type(price))
+# print(type(True))
+
+
+# age = 23
+# old = False
+# a = None #None Type
+# print(type(old))
+# print(type(a))
+
+#single comments
+
+""" Multiline 
+comments
+"""
+
+
+
+
+
