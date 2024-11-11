1d1871b959593a65ae3c83ff338870a9a3963885
itsjubayer <jubayer.ah@gmail.com>
2024-11-11 17:09:11 +0600

    commit test

--- FirstProgram.py
+++ FirstProgram.py
@@ -1,30 +1,14 @@
-# name = "Shradha"
-# age = 23
-# price = 25.99
+name = "Shradha"
+age = 23
+price = 25.99
 
-# print("my name is : ", name)
-# print("my age is : ", age)
-
-
-# print(type(name))
-# print(type(age))
-# print(type(price))
-# print(type(True))
+print("my name is : ", name)
+print("my age is : ", age)
 
 
-# age = 23
-# old = False
-# a = None #None Type
-# print(type(old))
-# print(type(a))
+print(type(name))
+print(type(age))
+print(type(price))
+print(type(True))
 
-#single comments
-
-""" Multiline 
-comments
-"""
-
-
-
-
-
+# Datatypes: integers, String, Fload, Boolean, None
--- python_list.py
+++ python_list.py
@@ -218,8 +218,8 @@
 # print(list1)
 
 
-list1 = ["a", "b" , "c"]
-list2 = [1, 2, 3]
+# list1 = ["a", "b" , "c"]
+# list2 = [1, 2, 3]
 
-list1.extend(list2)
-print(list1)
+# list1.extend(list2)
+# print(list1)
