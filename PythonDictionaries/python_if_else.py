3c5782c27286b4d69cf979d1beef48697b1d893b
itsjubayer <jubayer.ah@gmail.com>
2024-11-14 22:50:32 +0600

    commit conflict

--- PythonDictionaries/python_if_else.py
+++ PythonDictionaries/python_if_else.py
@@ -22,8 +22,8 @@
 #   print("a is greater than b")
 
 
-a = 200
-b = 33
+# a = 200
+# b = 33
 # if b > a:
 #   print("b is greater than a")
 # else:
@@ -33,13 +33,20 @@ b = 33
 # if a > b: print("a is greater than b")
 
 
-a = 330
-b = 330
-print("A") if a > b else print("=") if a == b else print("B")
+# a = 330
+# b = 330
+# print("A") if a > b else print("=") if a == b else print("B")
 
 
-a = 200
-b = 33
-c = 500
-if a > b and c > a:
-  print("Both conditions are True")
+# a = 200
+# b = 33
+# c = 500
+# if a > b and c > a:
+#   print("Both conditions are True")
+
+
+a = 33
+b = 200
+
+if b > a:
+  pass
