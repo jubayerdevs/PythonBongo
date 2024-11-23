c1178f2d59123c869cb93ab344325204631dc2e6
jubayerdevs <jubayer.ah@gmail.com>
2024-11-23 22:04:58 +0600

    Add File Handling

---
+++ class9i/File Handling/python_file_open.py
@@ -0,0 +1,62 @@
+import os
+
+#f = open("demofile.txt")
+
+#f = open("demofile.txt", "rt")
+
+#print(f.read())
+
+# f = open("H:\\Course\\ReactDjangoPython\\Practics\\6\\class9i\\File Handling\\demofile.txt", "r")
+# print(f.read())
+
+
+# f = open("H:\\Course\\ReactDjangoPython\\Practics\\6\\class9i\\File Handling\\demofile.txt", "r")
+# print(f.read(5))
+
+# f = open("H:\\Course\\ReactDjangoPython\\Practics\\6\\class9i\\File Handling\\demofile.txt", "r")
+# print(f.readline())
+
+
+# f = open("H:\\Course\\ReactDjangoPython\\Practics\\6\\class9i\\File Handling\\demofile.txt", "r")
+# print(f.readline())
+# print(f.readline())
+
+
+# f = open("H:\\Course\\ReactDjangoPython\\Practics\\6\\class9i\\File Handling\\demofile.txt", "r")
+# for x in f:
+#   print(x)
+
+
+# f = open("H:\\Course\\ReactDjangoPython\\Practics\\6\\class9i\\File Handling\\demofile.txt", "r")
+# print(f.readline())
+# f.close()
+
+
+# Python File Write
+
+# f = open("H:\\Course\\ReactDjangoPython\\Practics\\6\\class9i\\File Handling\\demofile2.txt", "a")
+# f.write("Now the file has more content!")
+# f.close()
+
+# #open and read the file after the appending:
+# f = open("H:\\Course\\ReactDjangoPython\\Practics\\6\\class9i\\File Handling\\demofile2.txt", "r")
+# print(f.read())
+
+
+# f = open("H:\\Course\\ReactDjangoPython\\Practics\\6\\class9i\\File Handling\\myfile.txt", "x")
+# f = open("H:\\Course\\ReactDjangoPython\\Practics\\6\\class9i\\File Handling\\myfile.txt", "w")
+
+os.remove("H:\\Course\\ReactDjangoPython\\Practics\\6\\class9i\\File Handling\\demofile.txt")
+
+# import os
+# if os.path.exists("demofile.txt"):
+#   os.remove("demofile.txt")
+# else:
+#   print("The file does not exist")
+
+
+import os
+os.rmdir("myfolder")
+
+
+
